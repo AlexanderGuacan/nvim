3e5d304642df7d02a98adba47e53c621e2c51172
AlexanderGuacan <adguacan2003@gmail.com>
2026-02-17 18:46:06 -0500

    Disabled latex syntax highlighting of vimtex plugin

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,5 +4,7 @@ return {
 	init = function()
 		vim.g.vimtex_view_method = "zathura_simple"
 		vim.g.vimtex_compiler_latexmk = { out_dir = vim.fn.getcwd() .. "/build" }
+		vim.g.vimtex_syntax_enabled = 0
+		vim.g.vimtex_syntax_conceal_disable = 1
 	end,
 }
