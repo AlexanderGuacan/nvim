ec5e0989efaacdc5928649d4d3624f1dbb6bcd41
AlexanderGuacan <adguacan2003@gmail.com>
2026-06-06 09:56:36 -0500

    Auto link html tags and format react files

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,13 +1,28 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	opts = {
-		check_ts = true,
-		ts_config = {
-			lua = { "string" },
-			javascript = { "template_string" },
-			java = false,
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			check_ts = true,
+			ts_config = {
+				lua = { "string" },
+				javascript = { "template_string" },
+				java = false,
+			},
+		},
+	},
+
+	{
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			opts = {
+				enable_close = true,
+				enable_rename = true,
+				enable_close_on_slash = true,
+			},
 		},
 	},
 }
